93784e14fa389bad962e0411926356c977062263
Laine Hallot <lainehallot@gmail.com>
2016-08-29 17:10:41 -0400

    Started work in image compositor

--- src/PostTextMaker.java
+++ src/PostTextMaker.java
@@ -1,10 +1,28 @@
+import rita.RiLexicon;
+import rita.RiMarkov;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+import java.util.List;
+
 /**
  * Created by mhallot on 8/25/16.
  */
 public class PostTextMaker {
+    String memes = "eeeeee";
 
-    boolean memes = true;
-    String tweetString = "test bot please ignore";
+    void getTimeline() throws TwitterException {
+        Twitter twitter = TwitterFactory.getSingleton();
+        List<Status> statuses = twitter.getHomeTimeline();
+        for (Status status : statuses) {
+            System.out.println(status.getText());
+        }
 
+        /*RiMarkov e = new RiMarkov(1);
+        e.loadText(memes);
+        e.generateSentence();*/
+    }
 
 }
