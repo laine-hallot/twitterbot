82d07fe93ed622f8257928202f9aabc5f84508b2
meyerhallot <meyerwasup@gmail.com>
2016-10-01 11:29:24 -0400

    Found the proper java library for markov chains. No longer need to find a way to get python to interface with java.

--- src/MyBot.java
+++ src/MyBot.java
@@ -3,6 +3,8 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
+import java.io.IOException;
+
 public class MyBot{
 
 	public static void main(String[] args) throws TwitterException{
@@ -13,9 +15,10 @@ public class MyBot{
 		Twitter twitter = TwitterFactory.getSingleton();
 		PostTextMaker test = new PostTextMaker();
 
-
-		//Status status = twitter.updateStatus(bullshit.tweetString);
-		System.out.println(test.getTimeline());
-
-	 }
+		try {
+			test.makeMarkov();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/PostTextMaker.java
+++ src/PostTextMaker.java
@@ -1,16 +1,26 @@
-/*import rita.RiLexicon;
-import rita.RiMarkov;*/
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import org.jibble.jmegahal.*;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
  * Created by mhallot on 8/25/16.
  */
 public class PostTextMaker {
+
+
+    void makeMarkov() throws IOException{
+        JMegaHal hal = new JMegaHal();
+
+        hal.addDocument("file:///home/meyerhallot/IdeaProjects/memebot9000/feed.txt");
+        String sentenceOne = hal.getSentence();
+        System.out.println(sentenceOne);
+    }
+
     List getTimeline() throws TwitterException {
         Twitter twitter = TwitterFactory.getSingleton();
         List<Status> statuses = twitter.getHomeTimeline();
